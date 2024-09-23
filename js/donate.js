0a2eb94ac993e6302564c9af4339c210a23b8987
Sais-opu <saidulislamapu2024@gmail.com>
2024-09-23 19:55:46 +0600

    log

--- js/donate.js
+++ js/donate.js
@@ -1,22 +1,37 @@
 // common function
-
-function getFieldValueById(id) {
-    const addM =parseFloat(document.getElementById(id).value)
-    return addM; 
+// For input field
+function getInputFieldValueById(id) {
+    const addM = parseFloat(document.getElementById(id).value);
+    return addM;
 }
-
-document.getElementById('donate').addEventListener('click', function () {
-    const input_1 = getFieldValueById('input-1')
-    
-    for (let char = 0; char < input_1.length;char++) {
-        let each = input_1[char];
-        if (each >= typeof each === "number" && each < 0) {
-            
-            console.log(each);
+// for text field
+function getTextFieldValueById(id) {
+    const addText = parseFloat(document.getElementById(id).innerText);
+    return addText;
+}
+//<div id=" limit" class="font-bold px-2">5500</div>
+/** For noakhali
+ * donate-nok = donate button 
+ * input-1 - input form
+ * nok-money - coin
+ * 
+ */
+document.getElementById('donate-nok').addEventListener('click', function () {
+    const input_1 = getInputFieldValueById('input-1');
+    const cash = getTextFieldValueById('limit');
+    if (input_1>0 ) {
+        const nokMoney = getTextFieldValueById('nok-money');
+        const total = input_1 + nokMoney;
+        document.getElementById('nok-money').innerText = total;
+        const newCash = cash - input_1;
+        if (newCash >= 0) {
+            document.getElementById('limit').innerText = newCash;
         }
-        
-        
+        else {
+            alert('Not enough Money')
+        }
     }
-    console.log('false');
-    
+    else {
+        alert('ki korla');
+    }
 });
